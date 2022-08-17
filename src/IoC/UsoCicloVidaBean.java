5a7b6cb24cfd5afca6a35d52ec8f25fd6f986dba
Ricardo Presilla <rpresilla@initiumsoft.com>
2022-08-17 12:57:38 -0300

    Update main.

--- src/IoC/UsoCicloVidaBean.java
+++ src/IoC/UsoCicloVidaBean.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class UsoCicloVidaBean {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext3.xml");
 		DirectorEmpleado Juan = contexto.getBean("miEmpleado", DirectorEmpleado.class);
 		System.out.println(Juan.getTareas());
 		System.out.println(Juan.getInforme());
